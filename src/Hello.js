33d0040094809cbc1b17abb42c76452a8da297d1
unknown <you@example.comtob.moeller@gmx.de git config --global user.name Your>
2020-04-21 19:41:38 +0200

    first component with react

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <p>yayor!</p>
+            </div>
+        );
+    }
+}
+
+export default Hello;
